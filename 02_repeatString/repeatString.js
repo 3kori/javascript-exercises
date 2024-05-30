c45f255d4c53c6259ac3865c88d46318a94779bf
3kori <127242906+3kori@users.noreply.github.com>
2024-05-30 23:54:44 +0300

    Successfully run and completed second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(greeting, numOfRepeats) {
+    let string ='';
+    for(let i = 0; i < numOfRepeats; i++) {
+        string += greeting;
+    }
+    if (numOfRepeats < 0) {
+        return "ERROR";
+    };
+    return string;
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
