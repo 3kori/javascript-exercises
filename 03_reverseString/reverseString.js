a1c986aa6977b711942a7fd5048e380b7eddcbfe
3kori <127242906+3kori@users.noreply.github.com>
2024-05-31 00:53:04 +0300

    Successfully run and completed third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const spString = string.split("");
+    let rvString = spString.reverse();
+    let rvGreeting = rvString.join("");
+    return rvGreeting;
 };
 
 // Do not edit below this line
