06d1cea9fe9d4ee567dcecbac08b82b226efa699
3kori <127242906+3kori@users.noreply.github.com>
2024-06-01 17:42:07 +0300

    Successfully run and complete forth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(args, num1, num2, num3, num4) {
+    let result = args.filter(function(remArgs) {
+      return remArgs !== num1  && remArgs !== num2 && remArgs !== num3 && remArgs !== num4;
+    });
+    return result;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
