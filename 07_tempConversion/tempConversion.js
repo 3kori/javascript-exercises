907cce646208a98f693b1fb39d095cc964179ed4
3kori <127242906+3kori@users.noreply.github.com>
2024-06-05 15:44:39 +0300

    Successfully run and complete seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  let fToCelsius = (tempInFahrenheit -32) * (5/9);
+
+  if (fToCelsius === 0) {
+    return 0;
+  }
+  else {
+    return +fToCelsius.toFixed(1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  let cToFahrenheit = (tempInCelsius * (9/5)) + 32;
+
+  if (cToFahrenheit === 0) {
+    return 0;
+  }
+  else {
+    return +cToFahrenheit.toFixed(1);
+  }
 };
 
 // Do not edit below this line
