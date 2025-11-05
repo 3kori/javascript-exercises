86bb60272c354eba6d8bf9608aa170615391e3bd
3kori <127242906+3kori@users.noreply.github.com>
2025-11-06 02:19:54 +0300

    Successfully run test and complete exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const lettersNumbers = '1234567890abcdefghijklmnopqrstuvwxyz';
 
+    const spacelessString = string.toLowerCase().split('').filter(filterArray).join('');
+
+    function filterArray(characters) {
+        return lettersNumbers.includes(characters);
+    }
+
+    const reversedString = spacelessString.split('').reverse().join('');
+
+    if (spacelessString === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
